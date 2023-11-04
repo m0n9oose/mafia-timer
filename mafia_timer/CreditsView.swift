56d62fcaed6e477400a5e77c3efd56327c7374b7
ilya shcherbinin <m0n9oose@gmail.com>
2023-11-04 14:02:00 +0100

    Add GH repo to credits view

--- mafia_timer/CreditsView.swift
+++ mafia_timer/CreditsView.swift
@@ -12,33 +12,38 @@ struct CreditsView: View {
     
     let imageLink = "https://www.freepik.com/free-vector/mysterious-gangster-character_7275333.htm"
     let ideaLink = "https://mafia-timer.neonll.ru"
-    let imagesize = CGFloat(32)
+    let githubLink = "https://github.com/m0n9oose/mafia-timer"
+    let imagesize = CGFloat(36)
 
     var body: some View {
         VStack {
             VStack {
                 Spacer()
-                
-                HStack {
+
+                HStack(alignment: .center) {
                     Image("logo-n")
                         .resizable()
                         .frame(width: imagesize, height: imagesize)
-                        .scaledToFit()
                     
                     Link(destination: URL(string: imageLink)!) {
                         Text("Image: Freepik.com")
                         Image(systemName: "arrow.up.forward.app")
                     }
                 }
-                
+
                 Link(destination: URL(string: ideaLink)!) {
                     Text("Original idea")
                     Image(systemName: "arrow.up.forward.app")
-                }.padding(.top, 10)
-                
+                }.padding(.top)
+
+                Link(destination: URL(string: githubLink)!) {
+                    Text("Github")
+                    Image(systemName: "arrow.up.forward.app")
+                }.padding(.top)
+
                 Spacer()
             }
-            
+
             Button {
                 dismiss()
             } label: {
